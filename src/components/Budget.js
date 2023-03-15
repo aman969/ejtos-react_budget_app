d0bbbf13326c395f307cc4adbe9e66d81bc90c1e
aman969 <barsaiya.aman95@gmail.com>
2023-03-15 11:08:25 -0400

    updated

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,32 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, expenses,currency } = useContext(AppContext);
+    const [newbudget, setNewbudget]=useState(budget);
+    const totalExpenses= expenses.reduce((total,item) =>{
+        return (total +=item.cost);
+    },0);
+    const updateBudget=(event) => {
+        if (event.target.value>20000){
+            alert("The value cannot exceed"+currency +"20000");
+        }
+        else if(event.target.value<totalExpenses){
+            alert("you can not reduce budget value less than spending");
+        }
+        else{
+            setNewbudget(event.target.value);
+        }
+    }
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: {currency}</span>
+            <input type="number" 
+                   required  
+                   value={newbudget}
+                   max="20000"
+                   step="10"
+                   style={{ marginLeft: '2rem' , size: 10}}
+                   onChange={updateBudget}/>
         </div>
     );
 };
---
+++ src/components/Currency.js
@@ -0,0 +1,23 @@
+import React, {useContext} from 'react'
+import { AppContext } from '../context/AppContext';
+const Currency=() =>{
+    const { dispatch } = useContext(AppContext);
+    const changeCurrency = (event) => {
+        dispatch({
+            type: 'CHG_CURRENCY',
+            payload: event.target.value,
+        });
+    };
+    return(
+        <select className="selectpicker" data-style="btn-success" id="currencySelect" onChange={changeCurrency}>
+            <option defaultValue>Currency (£ Pound)</option>
+            <option value="$" name="dollar"> $ Dollar</option>
+            <option value="£" name="pound">£ Pound</option>
+            <option value="€" name="euro">€ Euro</option>
+            <option value="₹" name="rupee">₹ Ruppee</option>
+            </select>
+        
+    );
+};
+
+export default Currency;
